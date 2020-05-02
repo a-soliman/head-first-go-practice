85d43a445c48b9009db95cf562e51cd9724f7d5b
a-soliman <ahmed.soliman@programmer.net>
2020-05-02 13:16:40 -0700

    coded the avrage programe, taking a file of values

--- chapter-05/avrage/main.go
+++ chapter-05/avrage/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/a-soliman/head_first_go/chapter-05/datafile"
+)
 
 func main() {
-	numbers := [3]float64{71.8, 56.2, 89.5}
+	numbers, err := datafile.GetFloats("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	var sum float64 = 0
 
 	for _, number := range numbers {
